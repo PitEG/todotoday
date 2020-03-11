aa289b7016bbeb869ed79395cad02bebf0a2b366
PitEG <enriqueiigan@gmail.com>
2020-03-11 00:36:14 -0700

    Made a reference for what the file will look like

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,18 @@
 using System;
+using System.IO;
 
 public class Program {  
   public static void Main(string[] args) {
+    //input must be a file directory
+    if (args.Length != 1) {
+      Console.WriteLine("Invalid input!");
+      return;
+    }
+
+    string fileDirectory = args[0];
+    if (!File.Exists(fileDirectory)) {
+      Console.WriteLine("File directory does not exist!");
+    }
+    
   }
 }
